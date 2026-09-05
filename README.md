fe078a6d2ca80a65136383cc7a4e2a32c2b0d8e6
Darren Nguyen <dnguyenbusiness616@gmail.com>
2026-09-05 00:57:13 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Blog made with [Astro Micro 🔬](https://astro-micro.vercel.app/)
---
+++ src/consts.ts
@@ -0,0 +1,26 @@
+import type { Metadata, Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "Darren Nguyen",
+  DESCRIPTION: "Hello!",
+  EMAIL: "darrennguyen27@proton.me",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "GitHub",
+    HREF: "https://github.com/darrennguyen25",
+  },
+];
